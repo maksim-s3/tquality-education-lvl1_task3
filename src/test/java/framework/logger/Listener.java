f62b4f627dcd33bf7df83a333a8566724cd3f996
m.startsev <maksim-s3@mail.ru>
2023-02-01 09:54:21 +0500

    update

--- src/test/java/framework/logger/Listener.java
+++ src/test/java/framework/logger/Listener.java
@@ -5,10 +5,6 @@ import org.testng.ITestResult;
 
 public class Listener implements ITestListener {
 
-    public void onTestStart(ITestResult result) {
-        System.out.println("onTestStart(): " + result.getName());
-    }
-
     public void onTestFailure(ITestResult result) {
         AllureHelpers.takeScreenshot();
         AllureHelpers.takeLogFile();
